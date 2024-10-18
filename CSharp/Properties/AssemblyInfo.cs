5fe13a85d54893f816ff0e8f05a0e505f4837053
vintasoft <alex@vintasoft.com>
2024-10-18 12:16:23 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VintaSoft WPF DICOM Viewer Demo")]
 [assembly: AssemblyDescription("Shows how to view and annotate DICOM files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Llc.")]
+[assembly: AssemblyCompany("VintaSoft LLC")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,5 +22,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("12.5.6.1")]
-[assembly: AssemblyFileVersion("12.5.6.1")]
+[assembly: AssemblyVersion("12.5.7.1")]
+[assembly: AssemblyFileVersion("12.5.7.1")]
