ddbdd1c362b20d5fe34361b9d65576ea2924cdc2
vintasoft <alex@vintasoft.com>
2025-11-21 10:19:38 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("14.1.17.1")]
-[assembly: AssemblyFileVersion("14.1.17.1")]
+[assembly: AssemblyVersion("14.1.18.1")]
+[assembly: AssemblyFileVersion("14.1.18.1")]
